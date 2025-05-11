d2279fd1dd89ee1af1b2e37ca04fd6c5d57b702b
mcmanyika <mcmanyika@gmail.com>
2025-05-11 15:10:05 -0400

    changed youtube video

--- src/app/account/utils/youtube.js
+++ src/app/account/utils/youtube.js
@@ -7,7 +7,7 @@ function YouTubeVideo() {
     <div className="relative w-full pt-[56.25%]">
       <iframe
         className="absolute top-0 left-0 w-full h-full"
-        src="https://www.youtube.com/embed/EkKVJ57ekQg"
+        src="https://www.youtube.com/embed/5xUxhZdXmvs"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
