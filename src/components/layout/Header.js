a0b660ea4c106f17b772cbb17441836b3ab1e966
mcmanyika <mcmanyika@gmail.com>
2025-05-11 21:20:03 -0400

    reduced header font size

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -65,8 +65,8 @@ function Header() {
   return (
     <header className="bg-white border-b border-gray-200 px-4 py-3">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-gray-800">
-        Hello {user?.user_metadata?.full_name } 
+        <h1 className="text-lg font-bold text-gray-800">
+        Hello <label className="font-thin">,{user?.user_metadata?.full_name }</label> 
         </h1>
         <div className="relative">
           <button
