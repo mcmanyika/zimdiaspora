3320c2ff0686b0ad50a5a068e37b8cbb0620f876
mcmanyika <mcmanyika@gmail.com>
2025-05-15 12:49:48 -0400

    update

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -344,7 +344,7 @@ const Dashboard = () => {
 
             <button 
               onClick={() => setIsModalOpen(true)}
-              className="border border-gray-600 text-black hover:bg-gray-800 hover:border-white hover:text-white rounded-md px-4 py-2 flex items-center gap-2"
+              className="bg-red-600 border-gray-600 text-white hover:bg-gray-800 hover:border-white hover:text-white rounded-md px-4 py-2 flex items-center gap-2"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
