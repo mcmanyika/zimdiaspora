8366935e05604469ace081094d726631fc8abcfc
mcmanyika <mcmanyika@gmail.com>
2025-05-16 14:50:01 -0400

    update

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -12,5 +12,5 @@ export async function GET(request: Request) {
     }
 
     // URL to redirect to after sign in process completes
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(new URL('/account', request.url))
 } 
