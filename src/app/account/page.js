bc0165f2e8427240d96352d5a566f2cdd7daf53c
mcmanyika <mcmanyika@gmail.com>
2025-05-24 16:36:52 -0600

    hide category icon on mobile

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -514,7 +514,7 @@ const Dashboard = () => {
                         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                       </svg>
                     ) : (
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="hidden md:block h-5 w-5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={tab.icon} />
                       </svg>
                     )}
